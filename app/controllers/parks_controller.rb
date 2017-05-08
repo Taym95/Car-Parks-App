c5f05fef2d6a01c475494154b12d6e5a3bf700ae
Taym95 <haddadi.taym@gmail.com>
2017-05-08 22:02:21 +0100

    Add CORS Accept Headers in API

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,6 +1,8 @@
 class ParksController < ApplicationController
   before_action :set_park, only: [:show, :edit, :update, :destroy]
 
+  before_filter :cors_preflight_check
+  after_filter :cors_set_access_control_headers
   # GET /parks
   # GET /parks.json
   def index
@@ -71,4 +73,23 @@ class ParksController < ApplicationController
     def park_params
       params.require(:park).permit(:name, :address, :slug, :city, :available, :has_camera, :has_watchman, :zip_code, :district, :price_month, :main_picture, :lat, :lng)
     end
+
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+
+  def cors_preflight_check
+    if request.method == 'OPTIONS'
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
+      headers['Access-Control-Max-Age'] = '1728000'
+
+      render :text => '', :content_type => 'text/plain'
+    end
+  end
 end
